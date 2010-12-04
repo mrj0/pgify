710888fe0c11375bd20a4d0d1c237b5f93296505
Mike Johnson <mike@publicstatic.net>
2010-12-04 11:38:06 -0800

    fail if pgify fails

--- test/testcommon.sh
+++ test/testcommon.sh
@@ -7,7 +7,7 @@ error=test/error
 trap "rm -f \"$input\" \"$output\" \"$expected\" \"$error\"" EXIT
 
 function pgify() {
-    ./pgify $* -d -f "$input" > "$output" 2>"$error"
+    ./pgify $* -d -f "$input" > "$output" 2>"$error" || exit $?
     $diff "$expected" "$output"
     ret=$?
     if test "$ret" != "0"; then
